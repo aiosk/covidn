bac87936ef5a1695bc12db6067c6c3278ca7cfce
Andry Yosua <andry.yosua@pinteraktif.com>
2020-06-27 03:37:27 +0700

    fixup! update chunk for chartjs

--- web/src/js/libs/chartjs.js
+++ web/src/js/libs/chartjs.js
@@ -1,6 +1,6 @@
 const getFile = async (zone, periods = 7) => {
   let res = await fetch(
-    `https://raw.githubusercontent.com/aiosk/covidn/master/cli/dist/chartjs/chunks/${zone}-${periods}.json?_=${Date.now()}`
+    `https://raw.githubusercontent.com/aiosk/covidn/master/cli/dist/chartjs/${zone}-${periods}.json?_=${Date.now()}`
   );
   let resJson = await res.json();
 
