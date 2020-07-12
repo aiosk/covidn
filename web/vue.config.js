fddae0272c9b09b023a1199dc4471e99f9c4851c
Andry Yosua <andry.yosua@pinteraktif.com>
2020-07-13 04:16:39 +0700

    update optimation webpack settings

--- web/vue.config.js
+++ web/vue.config.js
@@ -1,6 +1,12 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === "production" ? "/covidn/" : "/",
   configureWebpack: {
+    optimization: {
+      splitChunks: {
+        minSize: 10000,
+        maxSize: 250000,
+      },
+    },
     externals: {
       lazyload: "LazyLoad",
       chartjs: "Chart",
